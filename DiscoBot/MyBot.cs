a14941def6e2689b5f82d01ed3138c91a4905406
Matt Braden <matt.braden73@gmail.com>
2017-11-17 15:54:45 -0500

    Initial commit

---
+++ DiscoBot/MyBot.cs
@@ -0,0 +1,106 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoBot
+{
+    class MyBot
+    {
+        DiscordClient discord;
+        public MyBot()
+        {
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '!';
+                x.AllowMentionPrefix = true;
+            });
+
+            var commands = discord.GetService<CommandService>();
+
+            commands.CreateCommand("commands")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("!debug - sends hello world\n!fish - sends an image of a fish\n!dissapointed - sends an image of a dissapointed man\n!angry - sends an angry face\n!bored - sends and image of pure boredom\n!dood - dood!\n!retarded - sends the dumbest image known to man\n!hype - sends the most hype image\n!ok - sends Lucoa from the other world just to agree with you\n!nut - sends the nuttiest nut ever nutted");
+                });
+
+            commands.CreateCommand("debug")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("Hello, world!");
+                });
+
+            commands.CreateCommand("fish")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendFile("emotes/fish.png");
+                });
+
+            commands.CreateCommand("dissapointed")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendFile("emotes/dissapointed.png");
+                });
+
+            commands.CreateCommand("angry")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendFile("emotes/anger.png");
+                });
+
+            commands.CreateCommand("bored")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendFile("emotes/bored.png");
+                });
+
+            commands.CreateCommand("dood")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendFile("emotes/dood.png");
+                });
+
+            commands.CreateCommand("retarded")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendFile("emotes/Dumb_Denki.png");
+                });
+
+            commands.CreateCommand("hype")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendFile("emotes/hypeFull.png");
+                });
+
+            commands.CreateCommand("ok")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendFile("emotes/ok.png");
+                });
+
+            commands.CreateCommand("nut")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendFile("emotes/psiNut.png");
+                });
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("MzgwODg5OTE4NjgwODU4NjI3.DO_NTA.Ydye58G3mCAVTVBtTd9DgNyCCVM", TokenType.Bot);
+            });
+        }
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
